049063313cebede30ab147553eec4bde2f8b49d1
Diwakar <karthick raj@Sathish>
2020-01-13 18:16:50 +0530

    updated gihub

---
+++ src/helloeworld/Gihub.java
@@ -0,0 +1,13 @@
+package helloeworld;
+
+public class Gihub {
+
+
+	public static void main (String args[])
+	{
+		/*System.setProperty("webdriver.chrome.driver","C:\\Users\\karthick raj\\Downloads\\chromedriver.exe");	
+WebDriver driver=new hromeDriver();*/
+		System.out.println("Hello world");
+	}
+
+}
